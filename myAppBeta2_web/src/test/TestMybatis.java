1d8e06bf4672879f4c5f7ba5828bc35e05b11aba
jiangqiulin <1178988014@qq.com>
2017-11-02 17:07:44 +0800

    新添加单元测试相关文件

---
+++ myAppBeta2_web/src/test/TestMybatis.java
@@ -0,0 +1,29 @@
+import com.jr.entity.User;
+import com.jr.service.UserService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author jql
+ * @create 2017-11-02 16:55
+ * @desc mybatis单元测试类
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContextTest.xml")
+public class TestMybatis {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testGetOneUser(){
+        String username = "测试";
+        User user = userService.getOneUserByName(username);
+        Assert.assertNull(user);
+    }
+
+}
